014fe1734be2d32849cfdce254c5c511602fbb69
kakashi <trishiragolder69@outlook.com>
2024-02-16 01:11:02 +0530

    script configs

---
+++ install.ts
@@ -0,0 +1,15 @@
+
+Deno.chdir(new URL("./src",import.meta.url));
+
+const HOME=Deno.env.get("HOME");
+
+
+for await(const { name,isFile } of Deno.readDir("./")) {
+  if(!isFile) continue;
+
+  const buff=await Deno.readFile(name);
+  await Deno.writeFile(`${HOME}/${name}`,buff);
+}
+
+
+
